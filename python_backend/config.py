d421dfb8e05e7f14621bbd26feee0f72ce138dfa
Nishchal Dangol <nishchaldangol1111@gmail.com>
2026-08-17 18:05:31 +0545

    Remove credentials from configuration

--- python_backend/config.py
+++ python_backend/config.py
@@ -4,15 +4,18 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", os.urandom(32).hex())
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", os.urandom(32).hex())
+
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME", "your_email@gmail.com")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "your_app_specific_password")
+
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "uploads")
-    TWILIO_ACCOUNT_SID = os.getenv("REMOVED")
-    TWILIO_AUTH_TOKEN = os.getenv("REMOVED_TWILIO_TOKEN")
-    TWILIO_PHONE_NUMBER = os.getenv("REMOVED_TWILIO_PHONE")
+
+    TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
+    TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+    TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 
 class DevelopmentConfig(Config):
     DEBUG = True
